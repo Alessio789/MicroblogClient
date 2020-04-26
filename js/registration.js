e4d3ab9595aa52f733776266fc582f764dbe3a71
Alessio789 <alessio.trentin3@gmail.com>
2020-04-26 14:14:22 +0200

    Implemented client registration

--- js/registration.js
+++ js/registration.js
@@ -1,10 +1,10 @@
 var APP = {
 
-    registration : function () {
+    registration: function () {
 
-        var email = document.getElementById("email").innerHTML;
-        var username = document.getElementById("username").innerHTML;
-        var password = document.getElementById("password").innerHTML
+        var email = document.getElementById("email").value;
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
 
         var jsonBody = JSON.stringify({
             "username": username,
@@ -12,9 +12,32 @@ var APP = {
             "password": password
         });
 
+        APP.addUser(jsonBody);
+
     },
 
-    init_registration : function () {
+    addUser: function (user) {
+
+        if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xmlhttp.onreadystatechange = function () {
+
+            if (this.readyState === 4 && this.status === 201) {
+
+                location.href = "../index.html";
+            }
+        }
+
+        xmlhttp.open("POST", "https://localhost:8443/Microblog/rest/users", true);
+        xmlhttp.setRequestHeader("Content-type", "application/json");
+        xmlhttp.send(user);
+    },
+
+    init_registration: function () {
         $("#registration").on("click", APP.registration);
     }
 }
